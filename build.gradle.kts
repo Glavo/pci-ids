90093ea9875cef8f6277fc64114b99e168b4e006
Glavo <zjx001202@gmail.com>
2025-05-08 13:30:02 +0800

    Release 0.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,8 @@ plugins {
 }
 
 group = "org.glavo"
-version = "0.4.0-SNAPSHOT"
+version = "0.4.0"// + "-SNAPSHOT"
+description = "A library for parsing pci.ids file"
 
 repositories {
     mavenCentral()
@@ -19,6 +20,11 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.tukaani:xz:1.10")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
 }
 
 tasks.withType<JavaCompile> {
@@ -32,4 +38,88 @@ tasks.test {
 
 tasks.withType<GenerateModuleMetadata> {
     enabled = false
+}
+
+tasks.javadoc {
+    (options as StandardJavadocDocletOptions).also {
+        it.encoding("UTF-8")
+        it.addStringOption("link", "https://docs.oracle.com/en/java/javase/24/docs/api/")
+        it.addBooleanOption("html5", true)
+        it.addStringOption("Xdoclint:none", "-quiet")
+    }
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    val task = this
+    subprojects {
+        if (this != project(":benchmark")) {
+            task.sourceSets(sourceSets.main.get())
+        }
+    }
+
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(false)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            version = project.version.toString()
+            artifactId = project.name
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+                url.set("https://github.com/Glavo/pci-ids")
+
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("glavo")
+                        name.set("Glavo")
+                        email.set("zjx001202@gmail.com")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/Glavo/pci-ids")
+                }
+            }
+        }
+    }
+}
+
+if (rootProject.ext.has("signing.key")) {
+    signing {
+        useInMemoryPgpKeys(
+            rootProject.ext["signing.keyId"].toString(),
+            rootProject.ext["signing.key"].toString(),
+            rootProject.ext["signing.password"].toString(),
+        )
+        sign(publishing.publications["maven"])
+    }
+}
+
+// ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository
+nexusPublishing {
+    repositories {
+        sonatype {
+            stagingProfileId.set(rootProject.ext["sonatypeStagingProfileId"].toString())
+            username.set(rootProject.ext["sonatypeUsername"].toString())
+            password.set(rootProject.ext["sonatypePassword"].toString())
+        }
+    }
 }
